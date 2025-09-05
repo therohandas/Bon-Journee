788725853e80b07e0b729e37a132aa547d53cf73
Rohan Das <das.rohan.1254@gmail.com>
2025-09-05 18:38:46 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,8 +47,6 @@ export default function Home() {
               </Card>
             ))}
           </div>
-            </Link>
-          </div>
         </div>
       </section>
     </PageTransition>
