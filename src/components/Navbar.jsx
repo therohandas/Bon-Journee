5b82db03a1f14e3296c5d19d1eb54e230ed6ec0b
Rohan Das <das.rohan.1254@gmail.com>
2025-09-05 18:27:32 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,10 @@ export default function Navbar(){
   return (
     <header className="navbar">
       <div className="container navbar-inner">
-        <Link to="/" className="brand">Smart Tourism</Link>
+        <Link to="/" className="brand">Bon Journee</Link>
         <nav className="nav-links">
           <NavLink to="/" className="nav-link">Home</NavLink>
           <NavLink to="/explore" className="nav-link">Explore</NavLink>
-          <a className="nav-link" href="https://github.com" target="_blank" rel="noreferrer">GitHub</a>
         </nav>
       </div>
     </header>
