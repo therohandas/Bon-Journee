013797f769b89506f6e19c593a861f9d8248f50e
Rohan Das <das.rohan.1254@gmail.com>
2025-09-13 16:20:46 +0530

    Update MapView.jsx

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,15 +1,41 @@
-export default function MapView() {
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix Leaflet default marker icon issue
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+export default function MapView({ latitude, longitude, name }) {
+  if (!latitude || !longitude) {
+    return <p className="text-red-500">Location not available.</p>;
+  }
+
   return (
-    <div style={{ width: "100%", height: "500px" }}>
-      <iframe
-        title="Google Maps Preview"
-        width="100%"
-        height="100%"
-        style={{ border: 0 }}
-        loading="lazy"
-        allowFullScreen
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117462.25684456554!2d85.7633!3d20.2961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1909d6a9d3e4e7%3A0xabcdef123456789!2sBhubaneswar!5e0!3m2!1sen!2sin!4v1694343098765"
-      ></iframe>
+    <div className="h-[400px] w-full mt-4 rounded-lg overflow-hidden shadow-md">
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={15}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[latitude, longitude]}>
+          <Popup>{name || "Selected Location"}</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
