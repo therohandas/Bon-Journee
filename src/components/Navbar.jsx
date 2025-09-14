1a0cf64f15122d55772533c5ea980b98393745e4
Rohan Das <das.rohan.1254@gmail.com>
2025-09-14 16:18:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar(){
   return (
     <header className="navbar">
       <div className="container navbar-inner">
-        <Link to="/" className="brand">Bon Journee</Link>
+        <Link to="/" className="brand">Bon Voyage</Link>
         <nav className="nav-links">
           <NavLink to="/" className="nav-link">Home</NavLink>
           <NavLink to="/explore" className="nav-link">Explore</NavLink>
